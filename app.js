e1afa2612a8c2e3c51e5c7aa6dec1e187e4d26f0
Andrew J. Lohr <ajlohr@us.ibm.com>
2017-11-29 14:35:21 -0500

    update readme

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights
 var appEnv = cfenv.getAppEnv();
 
 // get the bounded Bluemix service credentials
-// var watsonService = appEnv.getService("MyPersonalityInsights");
-// var username = watsonService.credentials.username;
-// var password = watsonService.credentials.password;
+var watsonService = appEnv.getService("MyPersonalityInsights");
+var username = watsonService.credentials.username;
+var password = watsonService.credentials.password;
 
-// var personality_insights = new PersonalityInsightsV3({
-// 	username: username,
-// 	password: password,
-// 	version_date: '2016-10-19'
-// });
+var personality_insights = new PersonalityInsightsV3({
+	username: username,
+	password: password,
+	version_date: '2016-10-19'
+});
 
 // Configure views
 var path = require('path');
@@ -33,22 +33,22 @@ app.get('/', function(req, res) {
 });
 
 // Handle HTTP POST request to the "/watson" endpoint
-// app.post('/watson', function(req, res) {
-// 	var bioText = req.body.bioText;
+app.post('/watson', function(req, res) {
+	var bioText = req.body.bioText;
 
-// 	// do some server side input checking
-// 	if(getWordCount(bioText) < 100){
-// 		return res.status(400).json({msg: "Your bio must be at least 100 words"});
-// 	}
+	// do some server side input checking
+	if(getWordCount(bioText) < 100){
+		return res.status(400).json({msg: "Your bio must be at least 100 words"});
+	}
 
-// 	// get the personality data from Watson using the bio text
-// 	getPersonality(bioText, function(err, personalityJSON){
-// 		if(err)
-// 			console.log('error getting watson personality', err);
-// 		else
-// 			res.json(personalityJSON);
-// 	})
-// });
+	// get the personality data from Watson using the bio text
+	getPersonality(bioText, function(err, personalityJSON){
+		if(err)
+			console.log('error getting watson personality', err);
+		else
+			res.json(personalityJSON);
+	})
+});
 
 // Simple function to get the word count of a string.
 function getWordCount(text){
@@ -58,24 +58,22 @@ function getWordCount(text){
 	if(replaced.length === 0){
 		return 0;
 	}
-	else{
-		return replaced.split(' ').length;
-	}
+	return replaced.split(' ').length;
 }
 
 // Use the watson package to made the call to watson. The response will be the personality JSON that Watson computes.
-// function getPersonality(text, callback){
-// 	personality_insights.profile({
-// 		text: text,
-// 		consumption_preferences: true
-// 	},
-// 	function (err, response) {
-// 		if (err)
-// 			return callback(err, null);
-// 		else
-// 			return callback(null, response);
-// 	});
-// }
+function getPersonality(text, callback){
+	personality_insights.profile({
+		text: text,
+		consumption_preferences: true
+	},
+	function (err, response) {
+		if (err)
+			return callback(err, null);
+		else
+			return callback(null, response);
+	});
+}
 
 http.listen(appEnv.port, appEnv.bind);
 console.log('App started on ' + appEnv.bind + ':' + appEnv.port);
